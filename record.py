ba82e83c5ac87a9fcdb3392e1c8fdd54674ca065
이현승 <77336664+gesal03@users.noreply.github.com>
2023-11-22 13:41:36 +0900

    녹화

---
+++ record.py
@@ -0,0 +1,97 @@
+import cv2
+
+# 녹화 시작 및 종료를 위한 상태 변환
+recording = False
+
+# mqtt_sub.py에서 녹화 종료를 위한 코드
+def stop_recording():
+    global recording
+    recording = False
+
+# mqtt_sub.py에서 녹화 시작을 위한 코드
+def start_recording():
+    global recording
+    recording = True
+    
+def run(status: bool):
+    if recording:
+        if status:
+            capGoalLine = cv2.VideoCapture("rtsp://admin:asdf1346@@192.168.0.57:554/stream1/out.h265")
+            capLeft = cv2.VideoCapture("rtsp://admin:asdf1346@@192.168.0.58:554/stream1/out.h265")
+            capRight = cv2.VideoCapture("rtsp://admin:asdf1346@@192.168.0.59:554/stream1/out.h265")
+        else:
+            capGoalLine = cv2.VideoCapture("rtsp://admin:asdf1346@@192.168.0.63:554/stream1/out.h265")
+            capLeft = cv2.VideoCapture("rtsp://admin:asdf1346@@192.168.0.62:554/stream1/out.h265")
+            capRight = cv2.VideoCapture("rtsp://admin:asdf1346@@192.168.0.61:554/stream1/out.h265")
+    
+        if(capGoalLine.isOpened()):
+            print("GoalLine OK")
+        if(capLeft.isOpened()):
+            print("Left OK")
+        if(capRight.isOpened()):
+            print("Right OK")
+    
+        fourcc = cv2.VideoWriter_fourcc(*'X264')  # 비디오 코덱 설정
+        # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        goalLineWidth = capGoalLine.get(cv2.CAP_PROP_FRAME_WIDTH)
+        goalLineHeight = capGoalLine.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        goalLineFps= capGoalLine.get(cv2.CAP_PROP_FPS)
+
+        camWidth = capLeft.get(cv2.CAP_PROP_FRAME_WIDTH)
+        camHeight = capLeft.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        camFps = capLeft.get(cv2.CAP_PROP_FPS)
+
+        print("capGoalLine: ", capGoalLine.get(cv2.CAP_PROP_FPS), ", ", capGoalLine.get(cv2.CAP_PROP_FRAME_WIDTH), ", ", capGoalLine.get(cv2. CAP_PROP_FRAME_HEIGHT))
+        print("capLeft: ", capLeft.get(cv2.CAP_PROP_FPS), ", ", capLeft.get(cv2.CAP_PROP_FRAME_WIDTH), ", ", capLeft.get(cv2.   CAP_PROP_FRAME_HEIGHT))
+        print("capRight: ", capRight.get(cv2.CAP_PROP_FPS), ", ", capRight.get(cv2.CAP_PROP_FRAME_WIDTH), ", ", capRight.get(cv2.       CAP_PROP_FRAME_HEIGHT))
+    
+        # 비디오 생성 객체
+        # 파일 이름, 코덱, 프레임 속도, 프레임 크기 설정
+        if status: 
+            outGoalLine = cv2.VideoWriter('leftOutput/goalline.mp4', fourcc, goalLineFps, (int(goalLineWidth), int(goalLineHeight)))     
+            outLeft = cv2.VideoWriter('leftOutput/left.mp4', fourcc, camFps, (int(camWidth), int(camHeight))) # 파일 이름, 코덱, 프레임 속도, 프레임 크기 설정
+            outRight = cv2.VideoWriter('leftOutput/right.mp4', fourcc, camFps, (int(camWidth), int(camHeight)))
+        else:  
+            outGoalLine = cv2.VideoWriter('rightOutput/goalline.mp4', fourcc, goalLineFps, (int(goalLineWidth), int(goalLineHeight)))     
+            outLeft = cv2.VideoWriter('rightOutput/left.mp4', fourcc, camFps, (int(camWidth), int(camHeight))) # 파일 이름, 코덱, 프레임 속도, 프레임 크기 설정
+            outRight = cv2.VideoWriter('rightOutput/right.mp4', fourcc, camFps, (int(camWidth), int(camHeight)))
+    
+    
+        while True:
+            # 프레임을 읽어옵니다.
+            goalLineRet, goalLineFrame = capGoalLine.read()
+            leftRet, leftFrame = capLeft.read()
+            RightRet, RightFrame = capRight.read()
+            print(goalLineRet, ", ", leftRet, ", ", RightRet)
+            if cv2.waitKey(1) & 0xFF == ord('q'):  # 'q' 키를 누르면 루프를 종료합니다.
+                break
+            if not goalLineRet:
+                print("Failed GoalLine.")
+                break
+            if not leftRet:
+                print("Failed left.")
+                break
+            if not RightRet:
+                print("Failed right.")
+                break
+            # stop_recording() 을 통해 recording이 False가 되면 종료
+            if not recording:
+                print("record.py: stop")
+                cv2.destroyAllWindows()  # 모든 이미지 창을 닫습니다.
+                break
+            
+            # 프레임을 화면에 표시합니다, 나중에 삭제 가능
+            cv2.imshow('GoalLineCam Recording', goalLineFrame)
+            cv2.imshow('LeftCam Recording', leftFrame)
+            cv2.imshow('RightCam Recording', RightFrame)
+            # 프레임을 녹화 파일에 추가합니다.
+            outGoalLine.write(goalLineFrame)
+            outLeft.write(leftFrame)
+            outRight.write(RightFrame)
+    
+        outGoalLine.release()
+        outLeft.release()
+        outRight.release()
+        capGoalLine.release()
+        capLeft.release()
+        capRight.release()
---
+++ runModule.py
@@ -0,0 +1,39 @@
+import record
+import paho.mqtt.client as mqtt
+import threading
+
+def run(status: bool):
+    mqtt_broker = "192.168.0.42"
+    topic = "record"
+
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        client.subscribe(topic)
+
+    def on_message(client, userdata, msg):
+        message = msg.payload.decode()
+        print("arrived: ", message)
+
+        if message == "start":
+            record.start_recording()
+            print("Start recording")
+
+        elif message == "end":
+            record.stop_recording()
+            print("Stop recording")
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(mqtt_broker, 1883, 60)
+
+    def mqtt_thread():
+        client.loop_forever()
+
+    mqtt_thread = threading.Thread(target=mqtt_thread)
+    mqtt_thread.daemon = True
+    mqtt_thread.start()
+
+
+    # 메인 스레드에서 녹화 루프를 실행합니다.
+    while True:
+        record.run(status)
